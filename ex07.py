4730d73e6fca28cf84f3ec7f51891116f3f01323
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-09-17 20:53:15 -0300

    ex07

---
+++ ex07.py
@@ -0,0 +1,8 @@
+ni = int(input('Digite um numero como inicio:')) 
+nf = int(input('Digite um numero como limite:'))
+nm = int(input('Digite um numero multiplo:'))
+n3=0
+while ni<=nf:    
+    n3 = ni * nm
+    ni = ni +1
+    print(n3)
