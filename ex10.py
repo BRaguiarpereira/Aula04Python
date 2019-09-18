548bbc3a8a662bdece2d4596f0b6252e525ef81f
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-09-18 18:19:10 -0300

    ex10
    https://www.onlinegdb.com/online_python_compiler

---
+++ ex10.py
@@ -0,0 +1,14 @@
+nota1='b'
+nota2='a'
+nota3='d'
+n1 =  int (input( ' n1: ' ))
+n2 =  int (input( ' n2: ' ))
+n3 = int(input('n3:'))
+cont =  0
+if nota1==n1:
+    cont=cont+1
+if nota2==n2:
+    cont=cont+1
+if nota3==n3:
+    cont=cont+1
+print('Vocé acertou{}'.format(cont))
