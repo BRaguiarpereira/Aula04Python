8097830594e1da07a692bd83f86d595353f9cfff
Brian aguiar pereira <54900526+BRaguiarpereira@users.noreply.github.com>
2019-09-20 18:02:51 -0300

    Update ex10.py

--- ex10.py
+++ ex10.py
@@ -1,14 +1,15 @@
-nota1='b'
-nota2='a'
-nota3='d'
-n1 =  int (input( ' n1: ' ))
-n2 =  int (input( ' n2: ' ))
-n3 = int(input('n3:'))
+  
+nota1 ='b'
+nota2 ='a'
+nota3 = 'd'
+n1 =   str(input( ' n1: ' ))
+n2 =   str(input( ' n2: ' ))
+n3 =  str(input( ' n3: ' ))
 cont =  0
-if nota1==n1:
-    cont=cont+1
-if nota2==n2:
-    cont=cont+1
-if nota3==n3:
-    cont=cont+1
-print('Vocé acertou{}'.format(cont))
+if nota1 == n1:
+    cont = cont + 1
+if nota2 == n2:
+    cont = cont + 1
+if nota3 == n3:
+    cont = cont + 1
+print ( ' Vocé acertou {} '. format(cont))
